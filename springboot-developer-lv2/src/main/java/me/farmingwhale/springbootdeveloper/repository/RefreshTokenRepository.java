170dd8dd7edfc175509ad3d3a497a301710b3f52
InfraWhale <gyqjajang@gmail.com>
2024-01-06 00:18:05 +0900

    20240106

---
+++ springboot-developer-lv2/src/main/java/me/farmingwhale/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package me.farmingwhale.springbootdeveloper.repository;
+
+import me.farmingwhale.springbootdeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
