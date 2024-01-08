6f922e62c03fe0095a4eb2504329d6c28a370ecc
InfraWhale <gyqjajang@gmail.com>
2024-01-08 21:50:01 +0900

    JPA 강의 프로젝트 생성

---
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,57 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // persistence.xml에서 설정한 값
+
+        EntityManager em =  emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 삽입
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
+
+            // 조회
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id= " + findMember.getId());
+//            System.out.println("findMember.name= " + findMember.getName());
+
+            // 삭제
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+
+            // 수정
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            //JPQL로 조회
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit(); // 작업 끝나면 커밋 무조건 해줘야함
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); // 자원 닫아주기
+        }
+        
+        emf.close(); // 자원 닫아주기
+    }
+}
