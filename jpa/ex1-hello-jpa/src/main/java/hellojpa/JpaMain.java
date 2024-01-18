0b1753d5c959cdfeed28a9cea6ebab147a3cfa81
InfraWhale <gyqjajang@gmail.com>
2024-01-18 22:30:54 +0900

    프록시

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,20 +17,33 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member1 = new Member();
+            member1.setUserName("member1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+            em.flush();
+            em.clear();
 
-            em.persist(member);
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); //Proxy
+            // refMember.getUserName(); // 강제 초기화
+
+            Hibernate.initialize(refMember); // 강제 초기화
+
+/*            System.out.println("getUserName = " + emf.getPersistenceUnitUtil().isLoaded(refMember)); // 초기화 여부*/
 
             tx.commit(); // 작업 끝나면 커밋 무조건 해줘야함 -> 이 시점에 db에 쿼리가 날아감
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close(); // 자원 닫아주기
         }
         
         emf.close(); // 자원 닫아주기
     }
+        private static void logic(Member m1, Member m2) {
+            System.out.println("m1 : " + (m1 instanceof Member) );
+            System.out.println("m1 : " + (m2 instanceof Member) );
+        }
 }
