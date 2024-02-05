3e09a8ed4cab3c3650f2d7673fe868e838972902
InfraWhale <gyqjajang@gmail.com>
2024-02-05 21:40:00 +0900

    JPA 활용 1 프로젝트 환경설정

---
+++ jpaActual/jpashop/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // EntityManager 얘가 주입해줌
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId(); // 커맨드와 쿼리 분리(member 객체 바로 가져오지 말고 id 정도만 들고오게)
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ jpaActual/jpashop/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) // junit한테 스프링 관련 테스트를 함을 알려줌
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository; // Member Repository 주입받음
+
+    @Test
+    @Transactional // entityManager를 통한 모든 데이터 변경은 항상 Transaction 안에서 이루어져야 함 / spring패키지꺼를 쓰는것을 권장함
+    @Rollback(false) // 테스트 코드의 경우 한 트랜잭션 후 자동 롤백, 데이터가 들어가는것을 확인하려면 false 지정 가능
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member); //ctrl + alt + v
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // 같은 영속성 컨텍스트 안에서 id값 같으면 둘은 같음
+        System.out.println("findMember == member: " + (findMember == member) );
+
+
+    }
+}
