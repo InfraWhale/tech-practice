90c44e070d6f46defc86bc229fa0ca0035c63081
InfraWhale <gyqjajang@gmail.com>
2024-02-06 18:43:05 +0900

    상품 도메인 개발

--- jpaActual/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpaActual/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,7 +18,7 @@ public abstract class Item {
     @Id
     @GeneratedValue
     @Column(name = "item_id")
-    private long id;
+    private Long id;
 
     private String name;
     private int price;
@@ -25,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==// 응집력을 위해 엔티티에 구현
+    
+    /*
+    * stock 증가
+    * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+    * stock 감소
+    * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
