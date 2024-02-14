01166d1107de88e98c3d4f6267016485840bc684
InfraWhale <gyqjajang@gmail.com>
2024-02-14 18:52:03 +0900

    API 개발 기본

--- jpaActual/jpashop/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpaActual/jpashop/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,12 +10,45 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 //@Controller @ResponseBody : 둘을 합치면 @RestController
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+    
+    // 이 경우 엔티티가 직접 노출되어버림
+    // 수정하려면 엔티티를 직접 수정해야되는데 이러면 수정사항이 더 많아짐
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { //@RequstBody : json data를 가져와서 Member에 매핑
@@ -33,11 +66,16 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
-    @PutMapping("/api/v/members/{id}")
+    @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(
             @PathVariable("id") Long id,
             @RequestBody @Valid UpdateMemberRequest request) {
 
+        memberService.update(id, request.getName());
+        // Member 객체를 반환해올 수도 있지만, 그렇게 하면 커맨드와 쿼리가 분리되지 못함
+        Member findMember = memberService.findOne(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
     @Data
