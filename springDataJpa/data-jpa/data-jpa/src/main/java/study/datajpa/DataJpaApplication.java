a1d5be0c3e0cddcdcc58ea9947a3bdf614e6167b
InfraWhale <gyqjajang@gmail.com>
2024-02-23 13:34:11 +0900

    메소드 이름으로 쿼리 생성

--- springDataJpa/data-jpa/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ springDataJpa/data-jpa/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+// @EnableJpaRepositories(basePackages = "경로") // Springboot가 알아서 잡아준다
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
