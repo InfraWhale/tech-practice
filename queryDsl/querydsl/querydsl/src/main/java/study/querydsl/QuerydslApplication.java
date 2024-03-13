2567705a7d2176d3676ab3b3ca11658b914da108
InfraWhale <gyqjajang@gmail.com>
2024-03-13 15:28:30 +0900

    querydsl spring data JPA repository

--- queryDsl/querydsl/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ queryDsl/querydsl/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,13 +1,24 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
+	}
+	
+	// 없으면
+	// org.springframework.beans.BeanInstantiationException: Failed to instantiate [study.querydsl.repository.MemberJpaRepository]:
+	// No default constructor found
+	@Bean // 스프링에 Bean 등록
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
