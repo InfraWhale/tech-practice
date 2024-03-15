a65189915ba8d1b0d7dd2deedacb0738604f99cb
InfraWhale <gyqjajang@gmail.com>
2024-03-15 11:34:15 +0900

    spring data JPA querydsl 기능

--- queryDsl/querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ queryDsl/querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import java.util.List;
@@ -119,5 +120,35 @@ class MemberRepositoryTest {
         Page<MemberTeamDto> result2 = memberRepository.searchPageComplex2(condition, pageRequest);
         assertThat(result2.getSize()).isEqualTo(3);
         assertThat(result2.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    // spring data JPA 방식에 조건절만 붙혀쓰는 방식
+    // left join 불가
+    // service 단 등이 dto등 자바 객채가 아닌 querydsl 객체에 의존하게 됨
+    // 이후에 코드가 복잡해지면 못쓰게 될 가능성이 있음
+    @Test
+    public void queryDslPredicateExecutorTest() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println("member1 = " + findMember);
+        }
     }
 }
