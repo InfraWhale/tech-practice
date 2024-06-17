002b8275aa0520c2539072d983306e834be14ef2
InfraWhale <gyqjajang@gmail.com>
2024-06-17 17:00:09 +0900

    스프링 기본편 섹션 3

--- spring-loadmap/core/src/main/java/hello/core/MemberApp.java
+++ spring-loadmap/core/src/main/java/hello/core/MemberApp.java
@@ -1,35 +1,30 @@
 package hello.core;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import hello.core.order.Order;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class OrderApp {
+public class MemberApp {
 
     public static void main(String[] args) {
-
+        
+        // 직접 memberService 객체를 만드는게 아니라, AppConfig가 만들어줌
 /*        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();*/
+        MemberService memberService = appConfig.memberService();*/
 
         //@Bean 들을 이 컨테이너가 관리해 줌
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
-        Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
+        Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-
-        System.out.println("order = " + order);
-        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
     }
 }
--- spring-loadmap/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-loadmap/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,31 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    // // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 할인정책이 변경되었다.
 
+    // 현재 추상클래스 뿐만 아니라 구현클래스인 FixDiscountPolicy도 의존중 -> DIP 위반
+    // 그래서 RateDiscountPolicy를 쓰고싶다면, 코드를 변경해야 한다. -> OCP 위반
+
+    // 그래서 어떻게? 이렇게 한다.
+    private final DiscountPolicy discountPolicy;
+
+    // 단 이렇게 하면 NPE 발생한다.
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
