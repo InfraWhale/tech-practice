640a5cd92801bd703b7da29f6f37d4e5a854a61a
InfraWhale <gyqjajang@gmail.com>
2024-06-28 18:41:21 +0900

    스프링 MVC 1편 섹션 4

---
+++ jungle-practice/ch2/q1.js
@@ -0,0 +1,13 @@
+let n = 100;
+
+function q1(a) {
+  let sum = 0;
+
+  for (let i = 1; i <= a; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(q1(n));
---
+++ spring-loadmap/servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,66 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// urlPatterns : *에 뭐가 들어오든 해당 servlet이 호출됨
+@WebServlet(name="FrontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+
+        ControllerV3 controller = controllerMap.get(requestURI);
+        if(controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //paramMap
+        Map<String, String> paramMap = createParamMap(request); //request의 파라미터를 map으로 생성
+        ModelView mv = controller.process(paramMap); // map을 넘기면서 controller의 비즈니스로직 실행해서 ModelView로 반환
+
+        String viewName = mv.getViewName(); //논리이름 new-form
+
+        // /WEB-INF/views/new-form.jsp
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response); //render 호출
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
