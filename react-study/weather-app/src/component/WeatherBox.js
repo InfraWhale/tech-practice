937ffcad397e37a596b04bc77d5cfd183c367c03
Hyobeom Jang <gyqjajang@gmail.com>
2025-04-04 14:48:21 +0900

    날씨앱 UI 다듬기

--- react-study/weather-app/src/component/WeatherBox.js
+++ react-study/weather-app/src/component/WeatherBox.js
@@ -1,14 +1,45 @@
 import React from 'react'
 
-const WeatherBox = ({weather}) => {
-  // console.log("weather : ", weather)
+const WeatherBox = ({ weather }) => {
+  if (!weather) {
+    return (
+      <div className="weather-box">
+        <h3>날씨 정보를 조회하지 못했습니다.</h3>
+      </div>
+    );
+  }
+  console.log(weather);
+  const weatherMain = weather?.weather[0].main;
+  const emojiMap = {
+    Clear: "☀️",
+    Clouds: "☁️",
+    Rain: "🌧️",
+    Drizzle: "🌦️",
+    Thunderstorm: "⛈️",
+    Snow: "❄️",
+    Mist: "🌫️",
+    Smoke: "💨",
+    Haze: "🌁",
+    Dust: "🌬️",
+    Fog: "🌫️",
+    Sand: "🏜️",
+    Ash: "🌋",
+    Squall: "🌪️",
+    Tornado: "🌪️"
+  };
+
+  const emoji = emojiMap[weatherMain] || "🌈"; // 못 찾으면 기본값
+
   return (
     <div className="weather-box">
-        <div>{weather?.name}</div>
-        <h2>{weather?.main.temp.toFixed(1)}C / {(weather?.main.temp * 1.8 + 32).toFixed(1)}F</h2>
-        <h3>{weather?.weather[0].description}</h3>
+      <h3>🏙 {weather?.name}</h3>
+      <h2>
+         {weather?.main.temp.toFixed(1)} °C / {(weather?.main.temp * 1.8 + 32).toFixed(1)} °F
+      </h2>
+      <h3>{emoji} {weather?.weather[0].description}</h3>
     </div>
-  )
-}
+  );
+};
+
 
 export default WeatherBox
--- react-study/weather-app/src/component/WeatherButton.js
+++ react-study/weather-app/src/component/WeatherButton.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 const WeatherButton = ({ cities, city, setCity }) => {
-    //console.log(cities);
-    return (
-    <div>
-        <Button 
-            variant={city === "" ? "primary" : "warning"}
-            onClick={ () => setCity("")} 
-        >Current Location</Button>
-        {cities.map((item) => (
-            <Button 
-                variant={city === item ? "primary" : "warning"}
-                onClick={ () => setCity(item)}
-            >{item}</Button>
-        ))}
+  return (
+    <div className="weather-buttons">
+      <Button
+        variant={city === "" ? "primary" : "outline-light"}
+        onClick={() => setCity("")}
+      >
+        Current Location
+      </Button>
+      {cities.map((item, index) => (
+        <Button
+          key={index}
+          variant={city === item ? "primary" : "outline-light"}
+          onClick={() => setCity(item)}
+        >
+          {item}
+        </Button>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default WeatherButton
+export default WeatherButton;
