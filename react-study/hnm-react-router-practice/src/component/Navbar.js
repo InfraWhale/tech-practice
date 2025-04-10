edfea1eabf02543a6100201712a8640a93f02d51
Hyobeom Jang <gyqjajang@gmail.com>
2025-04-10 21:40:51 +0900

    모바일 버젼

--- react-study/hnm-react-router-practice/src/component/Navbar.js
+++ react-study/hnm-react-router-practice/src/component/Navbar.js
@@ -1,61 +1,86 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-regular-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import '../css/Navbar.css'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import '../css/Navbar.css';
 import { useNavigate } from 'react-router-dom';
 import mashopLogo from '../image/mashop.png';
 
 const Navbar = ({ authenticate, setAuthenticate }) => {
-    const menuList = ['광고상품', '남성복', '상남자관', 'Sale' ];
-
+    const menuList = ['광고상품', '남성복', '상남자관', 'Sale'];
+    const [menuOpen, setMenuOpen] = useState(false);
     const navigate = useNavigate();
 
     const goToLogin = () => {
-        navigate('/login')
-    }
+        navigate('/login');
+    };
 
     const handleLogout = () => {
         setAuthenticate(false);
     };
 
     const search = (event) => {
-        if(event.key === "Enter") {
-            // 입력한 검색어를 읽어와서
+        if (event.key === 'Enter') {
             let keyword = event.target.value;
-            console.log("keyword : ", keyword);
-            // url을 바꿔준다.
             navigate(`/?q=${keyword}`);
         }
-    }
-  return (
-    <div>
-        <div>
-            <div className='login-button' onClick={authenticate ? handleLogout : goToLogin}>
+    };
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    return (
+        <div className="navbar-wrapper">
+            {/* 햄버거 아이콘 */}
+            <div className="hamburger-icon" onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faBars} size="2x" />
+            </div>
+
+            {/* 로그인 버튼 */}
+            <div className="login-button" onClick={authenticate ? handleLogout : goToLogin}>
                 <FontAwesomeIcon icon={faUser} />
                 <div>{authenticate ? '로그아웃' : '로그인'}</div>
             </div>
-        </div>
-        <div>
-            <div className='nav-section'>
+
+            {/* 로고 */}
+            <div className="nav-section">
                 <img width={200} src={mashopLogo} onClick={() => navigate('/')} />
             </div>
-        </div>
-        <div className='menu-area'>
-            <div className="menu-left" />
-            <ul className='menu-list'>
-                {menuList.map((menu, idx) => <li key={idx}>{menu}</li>)}
-            </ul>
-            <div className="menu-right">
-                <div className='search-area'>
-                    <FontAwesomeIcon icon={faSearch}/>
-                    <input type="text" placeholder="검색어를 입력하세요" onKeyUp={(event) => search(event)}/>
+
+            {/* PC 메뉴 */}
+            <div className="menu-area">
+                <div className="menu-left" />
+                <ul className="menu-list">
+                    {menuList.map((menu, idx) => (
+                        <li key={idx}>{menu}</li>
+                    ))}
+                </ul>
+                <div className="menu-right">
+                    <div className="search-area">
+                        <FontAwesomeIcon icon={faSearch} />
+                        <input type="text" placeholder="검색어를 입력하세요" onKeyUp={search} />
+                    </div>
                 </div>
             </div>
+
+            {/* 모바일 오버레이 */}
+            {menuOpen && <div className="overlay" onClick={closeMenu}></div>}
+
+            {/* 모바일 메뉴 */}
+            <div className={`mobile-menu-slide ${menuOpen ? 'open' : ''}`}>
+                <ul className="mobile-menu-list">
+                    {menuList.map((menu, idx) => (
+                        <li key={idx} onClick={closeMenu}>{menu}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
--- react-study/hnm-react-router-practice/src/page/Login.js
+++ react-study/hnm-react-router-practice/src/page/Login.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Login.css';
 import { Button, Container, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 const Login = ({ setAuthenticate }) => {
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false); // 비밀번호 보기 토글
 
   const loginUser = (event) => {
     event.preventDefault();
     setAuthenticate(true);
-  }
+    navigate('/');
+  };
 
   return (
-    <Container>
-      <Form onSubmit={(event) => loginUser(event)}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>이메일</Form.Label>
-          <Form.Control type="email" placeholder="Email" />
-        </Form.Group>
+    <div className="login-wrapper">
+      <div className="login-box">
+        <h1>로그인</h1>
+        <Form onSubmit={loginUser}>
+          {/* 이메일 */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>이메일 <span className="required">*</span></Form.Label>
+            <Form.Control type="email" placeholder="email" className="custom-input" required />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>비밀번호</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="로그인 정보 저장" />
-        </Form.Group>
-        <Button variant="danger" type="submit">
-          로그인
-        </Button>
-      </Form>
-    </Container>
+          {/* 비밀번호 */}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>비밀번호 <span className="required">*</span></Form.Label>
+            <div className="password-wrapper">
+              <Form.Control
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="custom-input"
+                required
+              />
+              <button
+                type="button"
+                className="show-button"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? "HIDE" : "SHOW"}
+              </button>
+            </div>
+          </Form.Group>
+
+          {/* 옵션 */}
+          <Form.Group className="mb-3 d-flex justify-content-between align-items-center options">
+            <Form.Check type="checkbox" label="로그인 상태 유지" />
+            <a href="#">비밀번호를 잊으셨나요?</a>
+          </Form.Group>
+
+          {/* 버튼 */}
+          <Button type="submit" className="login-btn w-100">
+            로그인
+          </Button>
+          <Button type="button" className="signup-btn w-100 mt-2">
+            회원 가입하기
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 };
 
